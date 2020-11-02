ef6efef27c5fe7921d40f7848da44f67d81991dd
mbuettner1 <62313588+mbuettner1@users.noreply.github.com>
2020-11-02 17:16:08 -0500

    Google Map and Open Weather API's map linked to the "MAP" in Navbar.

--- app/src/main/java/com/example/fullweatherapp/MainActivity.java
+++ app/src/main/java/com/example/fullweatherapp/MainActivity.java
@@ -8,7 +8,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.example.fullweatherapp.ui.dashboard.DashboardFragment;
 import com.example.fullweatherapp.ui.home.HomeFragment;
+import com.example.fullweatherapp.ui.notifications.NotificationsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,25 +19,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectedFragement = null;
 
-                switch (item.getItemId()) {
-                    case R.id.action_weather:
-                        selectedFragement = new HomeFragment();
-                        break;
-                    case R.id.action_map:
-                        Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.action_history:
-                        Toast.makeText(MainActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
-                        break;                }
-                return true;
-            }
-        });
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.action_weather:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.action_map:
+                            selectedFragment = new DashboardFragment();
+                            break;
+                        case R.id.action_history:
+                            selectedFragment = new NotificationsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
--- app/src/main/java/com/example/fullweatherapp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/fullweatherapp/ui/dashboard/DashboardFragment.java
@@ -12,24 +12,65 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.fullweatherapp.MainActivity;
 import com.example.fullweatherapp.R;
+import com.example.fullweatherapp.ui.home.HomeFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.maps.model.TileProvider;
+import com.google.android.gms.maps.model.UrlTileProvider;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
+    }
+
+    private void onMapReady(GoogleMap googleMap) {
+        TileProvider tileProvider = new UrlTileProvider(256, 256) {
+            @Override
+            public URL getTileUrl(int x, int y, int zoom) {            /* Define the URL pattern for the tile images */
+                String s = String.format("https://tile.openweathermap.org/map/temp_new/%d/%d/%d.png?appid=Your-API-Key (Links to an external site.)", zoom, x, y);
+                if (!checkTileExists(x, y, zoom)) {
+                    return null;
+                }            try {
+                    return new URL(s);
+                } catch (MalformedURLException e) {
+                    throw new AssertionError(e);
+                }
+            }        /*
+             * Check that the tile server supports the requested x, y and zoom.
+             * Complete this stub according to the tile range you support.
+             * If you support a limited range of tiles at different zoom levels, then you
+             * need to define the supported x, y range at each zoom level.
+             */
+            private boolean checkTileExists(int x, int y, int zoom) {
+                int minZoom = 12;
+                int maxZoom = 16;            return (zoom >= minZoom && zoom <= maxZoom);
+            }
+        };
+
+
+        LatLng loc = new LatLng(151.211,-33.852 );
+        googleMap.addMarker(new MarkerOptions().position(loc).title("You are here!"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(12));
+        TileOverlay tileOverlay = googleMap.addTileOverlay(new TileOverlayOptions()
+                .tileProvider(tileProvider));
     }
 }
