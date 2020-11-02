f6f080ecc696bd01a211159207a2091dfbecc25c
mbuettner1 <62313588+mbuettner1@users.noreply.github.com>
2020-11-01 21:28:32 -0500

    Navigation bar created.

--- app/src/main/java/com/example/fullweatherapp/MainActivity.java
+++ app/src/main/java/com/example/fullweatherapp/MainActivity.java
@@ -1,30 +1,34 @@
 package com.example.fullweatherapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-    }
-
+public class MainActivity extends AppCompatActivity {    @Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+    bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_weather:
+                    Toast.makeText(MainActivity.this, "Recents", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.action_map:
+                    Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.action_history:
+                    Toast.makeText(MainActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
+                    break;                }
+            return true;
+        }
+    });
+}
 }
