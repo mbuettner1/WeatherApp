da52b29b414d58014ceb9ffdeac6a26af1ecd6e4
mbuettner1 <62313588+mbuettner1@users.noreply.github.com>
2020-11-02 12:59:36 -0500

    Speech to Text implemented in the HomeFragment.java and the microphone icon embedded into fragment_home.xml. The weather map can now convert speech to text in the search bar, and once "search" button is pressed, application searches weather for the spoken city/zip/gps.

--- app/src/main/java/com/example/fullweatherapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/fullweatherapp/ui/home/HomeFragment.java
@@ -1,11 +1,15 @@
 package com.example.fullweatherapp.ui.home;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,10 +36,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment {
 
+    private static final int REQ_CODE = 100;
     EditText editText;
     Button button;
     TextView country, city, temp, date, lat, lon, humidity, low, high, pressure, windSpeed;
@@ -71,6 +78,27 @@ public class HomeFragment extends Fragment {
                 findWeather();
             }
         });
+
+        ImageView speak = root.findViewById(R.id.speak);
+        speak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Need to speak");
+                try {
+                    startActivityForResult(intent, REQ_CODE);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            "Sorry your device not supported",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        
 
         return root;
     }
@@ -168,6 +196,19 @@ public class HomeFragment extends Fragment {
         }
     }
 
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_CODE: {
+                if (resultCode == getActivity().RESULT_OK && null != data) {
+                    ArrayList result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    editText.setText((CharSequence)result.get(0));
+                }
+                break;
+            }
+        }
+    }
 
 }
